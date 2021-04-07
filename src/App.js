f8e23b4e435b4592e2eb8c7bffa6defe4a514a66
ssuyash <kumarsuyash94@gmail.com>
2021-04-07 09:55:20 +0530

    lifecycle update

--- src/App.js
+++ src/App.js
@@ -8,17 +8,39 @@ import Form from './Components/Form'
 import Balancesheet from './Containers/Balancesheet'
 
 export default class App extends Component {
+  constructor(props) {
+    console.log("APP : Constructor")
+
+    super(props)
+  
+    this.state = {
+       count:0,
+       showHome:true
+    }
+
+  }
+  
   render() {
+    console.log("APP : render")
 
     return (
-      <Router>
-        <Route path="/" component={Home} exact/>
-        <Route path="/balance" component={Balancesheet}/>
-        <Route path="/calc" component={Calc}/>
-      </Router>
+      // <Router>
+      //   <Route path="/" component={Home} exact/>
+      //   <Route path="/balance" component={Balancesheet}/>
+      //   <Route path="/calc" component={Calc}/>
+      // </Router>
+    
+<>
+      {this.state.showHome ? <Home/> : 
+    <h1>App component {this.state.count}</h1>}
+
+    <button onClick={()=>this.setState({showHome:false})}>hide home</button>
+   </>
+      
       
     )
 
+    
     // let persons = [
     //   {name:"Anukriti", gender:"female", yob:"1996"},
     //   {name:"Alpna", gender:"female", yob:"1995"},
@@ -38,4 +60,38 @@ export default class App extends Component {
     //   })
     // )
   }
+
+
+  static getDerivedStateFromProps(props, state){
+    console.log("APP : getDerivedStateFromProps")
+    return null
+      
+  }
+
+  componentDidMount(){
+    console.log("APP : componentDidMount")
+    setTimeout(()=>{
+      this.setState({count:1})
+    }, 2000)
+  }
+
+  // shouldComponentUpdate(newProps, newState){
+  //   console.log("APP : shouldcomponentupdate")
+  //   if(this.state.count != newState.count){
+  //     return true
+  //   }
+  //   return false
+
+  // }
+
+  getSnapshotBeforeUpdate(oldProps, oldState){
+    console.log("APP : getsnapshotbeforeupdate")
+    return {...oldState}
+  }
+
+  componentDidUpdate(oldProps, oldState, snapshot){
+    console.log(snapshot)
+    console.log("APP : componentdidupdate")
+  }
+
 }
--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -2,21 +2,62 @@ import React, { Component } from 'react'
 import Nav from '../Components/Nav'
 import Carousel from '../Components/Carousel'
 
+
+
 export default class Home extends Component {
+    constructor(props) {
+        console.log("HOME : constructor")
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
     render() {
+        console.log("HOME : render")
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-md-12">
-                        <Nav/>
+                        <h1>HOme component</h1>
+                        {/* <Nav/> */}
                     </div>
 
-                    <div className="col-md-12">
+                    {/* <div className="col-md-12">
 <Carousel/>
-                    </div>
+                    </div> */}
 
                 </div>
             </div>
         )
     }
+
+    static getDerivedStateFromProps(props, state){
+        console.log("HOME : getDerivedStateFromProps")
+        return null
+    }
+
+    componentDidMount(){
+        console.log("HOME : componentdidmount")
+    }
+
+
+    shouldComponentUpdate(newProps, newState){
+        console.log("HOME : shouldcomponentupdate")
+        return true
+      }
+    
+      getSnapshotBeforeUpdate(oldProps, oldState){
+        console.log("HOME : getsnapshotbeforeupdate")
+        return null
+      }
+    
+      componentDidUpdate(oldProps, oldState, snapshot){
+        console.log("HOME : componentdidupdate")
+      }
+      componentWillUnmount(){
+        console.log("HOME : componentwillunmount")
+
+      }
 }
